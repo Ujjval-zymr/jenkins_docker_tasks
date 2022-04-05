37217a452f139dc12a3fa43f86aa896fc1158307
Ujjval-zymr <101690120+Ujjval-zymr@users.noreply.github.com>
2022-04-05 12:59:31 +0530

    Delete shell_scripts directory

--- shell_scripts/3read_file.bash
+++
@@ -1,8 +0,0 @@
-# Read that above file and print those keys and values in vertical manner side by side.
-
-#!/bin/bash
-file='keyvalpair.txt'
-#while read line; do
-#echo $line 
-#done < $file
-cat $file
